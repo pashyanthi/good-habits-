3ff4f7794cad80c1765b4396fb6cf853ef3e235b
pashyanthi <67546604+pashyanthi@users.noreply.github.com>
2020-07-23 10:19:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var cPaper,binpart1,binpart2,binpart3,ground
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	binpart1=new Dustbin(600,630,200,20);
+	binpart2=new Dustbin(500,590,20,100)
+	binpart3=new Dustbin(700,590,20,100);
+	cPaper= new paper(80,610);
+	ground=new Ground(150,650,1300,10)
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  binpart1.display();
+  binpart2.display();
+  binpart3.display();
+  ground.display();
+  cPaper.display();
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+	if(keyCode === UP_ARROW){
+		Matter.Body.applyForce(cPaper.body,cPaper.body.position,{x:30,y:-30});
+	}
+
+}
+
+
+
